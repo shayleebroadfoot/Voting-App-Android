5dce939df4844abcae38203e0705de9b11508a9f
shayleebroadfoot <shayleebroadfoot@gmail.com>
2024-03-23 17:10:00 -0700

    Login and sign up pages working (no database)

--- app/src/main/java/com/example/votingappproject/SignupActivity.java
+++ app/src/main/java/com/example/votingappproject/SignupActivity.java
@@ -5,26 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.example.votingappproject.model.User;
 
 public class SignupActivity extends AppCompatActivity {
 
-    TextView txtSignIn;
+    //TextView txtSignIn;
+    FirebaseDatabase database;
+    int initialId = 0;
+
+    DatabaseReference databaseReference_user;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+    public void createUser(View view)
+    {
+        EditText usernameEditText = findViewById(R.id.text_username);
+        EditText passwordEditText = findViewById(R.id.text_password);
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
 
-        txtSignIn = findViewById(R.id.txtlogs);
+        User newUser = new User(username, password);
 
-        txtSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        database = FirebaseDatabase.getInstance("https://votingapp-6475d-default-rtdb.firebaseio.com/");
+        databaseReference_user = database.getReference("Users");
+        databaseReference_user.child(newUser.toString()).setValue(newUser);
+
+        displayDashboard();
+    }
+
+    public void displayDashboard()
+    {
+        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
